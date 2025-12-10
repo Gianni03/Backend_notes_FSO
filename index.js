12d215f002c42d5f4af669269031d0f0bb9d07e1
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-12-09 22:18:16 -0300

    manejo de errorer

--- index.js
+++ index.js
@@ -1,47 +1,10 @@
 require('dotenv').config()
 const express = require('express')
+const Note = require('./models/note')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const Note = require('./models/note')
-
-// const password = process.argv[2]
-
-// const url = `mongodb+srv://giannipasquinelli_db_user:${password}@clusterfso.kfxeyyr.mongodb.net/noteApp?appName=ClusterFSO`
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGODB_URI)
-
-const noteSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
-
-// const Note = mongoose.model('Note', noteSchema)
-
 
 const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(express.static('dist'))
-
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
-
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -51,8 +14,29 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGODB_URI)
+
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
+app.use(cors())
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -65,16 +49,38 @@ app.get('/api/notes', (request, response) => {
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -108,6 +114,10 @@ noteSchema.set('toJSON', {
     delete returnedObject.__v
   }
 })
+
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
