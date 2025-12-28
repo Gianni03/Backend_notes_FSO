c56693b12f099fed788bbcbca3be957d8a30342e
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-12-28 09:57:48 -0300

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,14 +9,16 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.node }, plugins: { js, stylistic },
     extends: ["js/recommended"]
   },
-  'rules': {
-    "eqeqeq": 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    'arrow-spacing': [
-      'error', { 'before': true, 'after': true }
-    ],
-    'no-console': 0
+  {
+    rules: {
+      "eqeqeq": 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': [
+        'error', { 'before': true, 'after': true }
+      ],
+      'no-console': 0
+    },
   },
   globalIgnores(['./dist/'])
 ]);
